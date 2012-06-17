afab3b5bfc34374c465651480db3d9c93ad873b5
Adam Hintz <adam.o.dev@gmail.com>
2012-06-17 09:41:03 -0700

    Stories not connected yet, but the files are there

---
+++ Classes/ShareStoryViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareStoryViewController.h
+//  iFixit
+//
+//  Created by Adam Hintz on 6/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "StarRatingControl.h"
+
+@interface ShareStoryViewController : UIViewController <StarRatingDelegate>
+
+@end
---
+++ Classes/ShareStoryViewController.m
@@ -0,0 +1,68 @@
+//
+//  ShareStoryViewController.m
+//  iFixit
+//
+//  Created by Adam Hintz on 6/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ShareStoryViewController.h"
+
+@implementation ShareStoryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (void)starRatingControl:(StarRatingControl *)control didUpdateRating:(NSUInteger)rating {
+	// Call back to indicate the control is currently being updated
+}
+
+- (void)starRatingControl:(StarRatingControl *)control willUpdateRating:(NSUInteger)rating {
+	
+}
+
+@end
